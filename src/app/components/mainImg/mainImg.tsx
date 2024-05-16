1592cb8820bf291ee324c18922f40b41be6d6c70
sobokki <jso4093@naver.com>
2024-05-16 17:08:35 +0900

    Feat: 네브 컴포넌트, 모달 컴포넌트, 홈 페이지 완성

---
+++ src/app/components/mainImg/mainImg.tsx
@@ -0,0 +1,17 @@
+'use client';
+import { usePathname } from 'next/navigation';
+import Image from "next/image";
+import photo from "../../../../public/mainImg.svg"
+import styles from "./mainImg.module.scss"
+
+export default function MainImg(){
+  const path = usePathname();
+  const showImageOnPaths = ["/","/about","/project","/visitor"];
+  return(
+    <>
+     {showImageOnPaths.includes(path)&&
+    <Image src={photo}  className={styles.mainImg} alt="메인 이미지"/>
+     }
+    </>
+  )
+}
---
+++ src/app/components/modal/modalContainer.tsx
@@ -0,0 +1,40 @@
+import { ReactNode, useEffect, useRef } from "react";
+import styles from "@/app/components/modal/modalContainer.module.scss";
+
+interface ModalContainerProps {
+  children: ReactNode;
+  className?: string;
+  closeClick:()=>void;
+}
+
+export default function ModalContainer({
+  children,
+  className,
+  closeClick,
+}: ModalContainerProps) {
+
+const modalOverlayRef = useRef<HTMLDivElement>(null);
+
+const handleClickOutsied =(e:MouseEvent)=>{
+  if(modalOverlayRef.current && modalOverlayRef.current === e.target){
+    closeClick();
+  }
+}
+
+useEffect(()=>{
+  document.addEventListener("mousedown",handleClickOutsied);
+  return()=>{
+    document.removeEventListener("mousedown", handleClickOutsied);
+  }
+},[]);
+  return (
+    <div ref={modalOverlayRef} className={styles["modal-overlay"]}>
+      <div className={`${styles["modal-container"]} ${className}`}>
+        <button className={styles["modal-close"]} onClick={closeClick}>
+          &times;
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+}
